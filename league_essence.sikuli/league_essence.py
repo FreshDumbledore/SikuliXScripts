2555fa43d20a4e99ff550ffbb3ddd097b046f89a
FreshDumbledore <maximilian.lasser@gmail.com>
2022-04-16 13:38:40 +0200

    Add files via upload

---
+++ league_essence.sikuli/league_essence.py
@@ -0,0 +1,8 @@
+while(True):
+    if exists(Pattern("1650108279107.png").targetOffset(24,83)):
+        click(getLastMatch())
+        if exists("1650108316038.png"):
+            click(getLastMatch())
+            if exists("1650108334552.png"):
+                click(getLastMatch())
+        
